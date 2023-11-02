bce5b47afec7a054e3ba60a4b43e2b8db39c9000
co-bby <nkumsagoefredrick@gmail.com>
2023-11-01 23:48:33 -0400

    fix same issue

---
+++ lib/src/ReadProgress.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+type progressBarProps = {
+    color: string;
+    height: number;
+    scrollOffset: number;
+    zIndex: number;
+};
+declare const ProgressBar: React.FC<progressBarProps>;
+export default ProgressBar;
